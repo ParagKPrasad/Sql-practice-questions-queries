b5962d2b8d3ec39b13cd50d216e64c4b3dd517e5
PARAG KUMAR PRASAD <142146437+ParagKPrasad@users.noreply.github.com>
2026-07-28 20:48:45 +0530

    Add files via upload

---
+++ MNC.sql
@@ -0,0 +1,46 @@
+create database MNC;
+use MNC;
+CREATE TABLE departments (
+dept_id INT PRIMARY KEY,
+dept_name VARCHAR(50),
+location VARCHAR(50)
+);
+INSERT INTO departments VALUES
+(10,'HR','Jaipur'),
+(20,'IT','Bangalore'),
+(30,'Finance','Mumbai'),
+(40,'Marketing','Delhi'),
+(50,'Legal','Pune');
+CREATE TABLE employees (
+emp_id INT PRIMARY KEY,
+emp_name VARCHAR(50),
+dept_id INT,
+salary INT,
+city VARCHAR(50)
+);
+INSERT INTO employees VALUES
+(101,'Amit',10,45000,'Jaipur'),
+(102,'Bhavna',20,72000,'Bangalore'),
+(103,'Chirag',20,68000,'Bangalore'),
+(104,'Divya',30,55000,'Mumbai'),
+(105,'Esha',NULL,39000,'Delhi'),
+(106,'Farhan',40,61000,'Delhi'),
+(107,'Gaurav',60,50000,'Kolkata'),
+(108,'Hina',30,83000,'Mumbai');
+CREATE TABLE projects (
+proj_id INT PRIMARY KEY,
+proj_name VARCHAR(50),
+dept_id INT,
+budget INT
+);
+INSERT INTO projects VALUES
+(1,'Payroll Revamp',10,120000),
+(2,'Cloud Migration',20,500000),
+(3,'Mobile App',20,300000),
+(4,'Audit Automation',30,250000),
+(5,'Brand Refresh',40,180000),
+(6,'Data Lake',70,400000);
+
+select * from departments;
+select * from employees;
+select * from projects;
